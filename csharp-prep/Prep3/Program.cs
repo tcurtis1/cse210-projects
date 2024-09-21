9f4b14b3282fc794284bb94c58d316ef7031afcc
tcurtis1 <tcurtis1@gmail.com>
2024-09-21 14:27:10 -0600

    prep 3 guessing game done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Console.Write("What is the magic number? ");
+        //int magic = int.Parse(Console.ReadLine());
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+        int letsGo = 1;
+        int guesses = 0;
+        Console.WriteLine("A random number between 1 and 100 has been chosen");
+
+        while (letsGo == 1) {
+            
+            Console.Write("What is your guess? ");
+            int guess = int.Parse(Console.ReadLine());
+
+            if (guess == number) {
+                letsGo = 0;
+            }
+            else if (guess < number) {
+                Console.WriteLine("Try a higher number next.");
+            }
+            else {
+                Console.WriteLine("Try a lower number next.");
+            }
+            guesses ++;
+        }
+        Console.WriteLine($"Congrats you guessed the number {number} correctly after {guesses} guesses!");
     }
 }
