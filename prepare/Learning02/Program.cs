ef68ceec3e6d1dab1173ad187a55688e94b34fb7
tcurtis1 <tcurtis1@gmail.com>
2024-09-25 17:52:44 -0600

    learning 02 complete

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        //Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        Job job2 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+        job2._company = "Apple";
+        job2._jobTitle = "Manager";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+
+        //Console.WriteLine($"{job1._company}\n{job2._company}");
+        //job1.DisplayJobDetails();
+        //job2.DisplayJobDetails();
+        Resume myResume = new Resume();
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        //Console.WriteLine(myResume._jobs[0]._jobTitle);
+        myResume.DisplayResume();
     }
+
 }
